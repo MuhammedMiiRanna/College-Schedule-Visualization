68466cf2501e4558a35558198e3006e994b42839
MuhammedMiiRanna <merali.med@gmail.com>
2022-12-09 11:31:05 +0100

    init

---
+++ dataCollect.py
@@ -0,0 +1,127 @@
+# la matière, le numéro de salle, le numéro du
+# groupe et/ou la section, et l’enseignant.
+
+days = {
+    0: 'Monday',
+    1: 'Tuesday',
+    2: 'Wednesday',
+    3: 'Thursday',
+    4: 'Friday',
+    5: 'Saturday',
+    6: 'Sunday'
+}
+
+timing = {
+    0: '08:00',
+    1: '09:30',
+    2: '11:20',
+    3: '13:00',
+    4: '14:40',
+    5: '16:10'
+}
+
+infos = {
+    0: 'Salle',
+    1: 'Grp',
+    2: 'Type',
+    3: 'Module',
+    4: 'Prof'
+}
+# N_module, N_grp, N_students, N_teachers, teachers_list
+sec_info = {
+    'M1IV': (),
+    'M2IV': ()}
+
+
+M1IV = {
+    'Sunday': {
+        '08:00': ((), (), 'Type', 'Module', ()),
+        '09:30': ((), (), 'Type', 'Module', ()),
+        '11:20': ((), (), 'Type', 'Module', ()),
+        '13:00': ((), (), 'Type', 'Module', ()),
+        '14:40': ((), (), 'Type', 'Module', ()),
+        '16:10': ((), (), 'Type', 'Module', ())
+    },
+    'Monday': {
+        '08:00': ((), (), 'Type', 'Module', ()),
+        '09:30': ((), (), 'Type', 'Module', ()),
+        '11:20': ((), (), 'Type', 'Module', ()),
+        '13:00': ((), (), 'Type', 'Module', ()),
+        '14:40': ((), (), 'Type', 'Module', ()),
+        '16:10': ((), (), 'Type', 'Module', ())
+    },
+    'Tuesday': {
+        '08:00': ((), (), 'Type', 'Module', ()),
+        '09:30': ((), (), 'Type', 'Module', ()),
+        '11:20': ((), (), 'Type', 'Module', ()),
+        '13:00': ((), (), 'Type', 'Module', ()),
+        '14:40': ((), (), 'Type', 'Module', ()),
+        '16:10': ((), (), 'Type', 'Module', ())
+    },
+    'Wednesday': {
+        '08:00': ((), (), 'Type', 'Module', ()),
+        '09:30': ((), (), 'Type', 'Module', ()),
+        '11:20': ((), (), 'Type', 'Module', ()),
+        '13:00': ((), (), 'Type', 'Module', ()),
+        '14:40': ((), (), 'Type', 'Module', ()),
+        '16:10': ((), (), 'Type', 'Module', ())
+    },
+    'Thursday': {
+        '08:00': ((), (), 'Type', 'Module', ()),
+        '09:30': ((), (), 'Type', 'Module', ()),
+        '11:20': ((), (), 'Type', 'Module', ()),
+        '13:00': ((), (), 'Type', 'Module', ()),
+        '14:40': ((), (), 'Type', 'Module', ()),
+        '16:10': ((), (), 'Type', 'Module', ())
+    },
+    'Friday': None,
+    'Saturday': None,
+}
+
+M2IV = {
+    'Sunday': {
+        '08:00': ((), (), 'Type', 'Module', ()),
+        '09:30': ((), (), 'Type', 'Module', ()),
+        '11:20': ((), (), 'Type', 'Module', ()),
+        '13:00': ((), (), 'Type', 'Module', ()),
+        '14:40': ((), (), 'Type', 'Module', ()),
+        '16:10': ((), (), 'Type', 'Module', ())
+    },
+    'Monday': {
+        '08:00': ((), (), 'Type', 'Module', ()),
+        '09:30': ((), (), 'Type', 'Module', ()),
+        '11:20': ((), (), 'Type', 'Module', ()),
+        '13:00': ((), (), 'Type', 'Module', ()),
+        '14:40': ((), (), 'Type', 'Module', ()),
+        '16:10': ((), (), 'Type', 'Module', ())
+    },
+    'Tuesday': {
+        '08:00': ((), (), 'Type', 'Module', ()),
+        '09:30': ((), (), 'Type', 'Module', ()),
+        '11:20': ((), (), 'Type', 'Module', ()),
+        '13:00': ((), (), 'Type', 'Module', ()),
+        '14:40': ((), (), 'Type', 'Module', ()),
+        '16:10': ((), (), 'Type', 'Module', ())
+    },
+    'Wednesday': {
+        '08:00': ((), (), 'Type', 'Module', ()),
+        '09:30': ((), (), 'Type', 'Module', ()),
+        '11:20': ((), (), 'Type', 'Module', ()),
+        '13:00': ((), (), 'Type', 'Module', ()),
+        '14:40': ((), (), 'Type', 'Module', ()),
+        '16:10': ((), (), 'Type', 'Module', ())
+    },
+    'Thursday': {
+        '08:00': ((), (), 'Type', 'Module', ()),
+        '09:30': ((), (), 'Type', 'Module', ()),
+        '11:20': ((), (), 'Type', 'Module', ()),
+        '13:00': ((), (), 'Type', 'Module', ()),
+        '14:40': ((), (), 'Type', 'Module', ()),
+        '16:10': ((), (), 'Type', 'Module', ())
+    },
+    'Friday': None,
+    'Saturday': None,
+}
+
+
+data = {'M1IV': M1IV, 'M2IV': M2IV}
