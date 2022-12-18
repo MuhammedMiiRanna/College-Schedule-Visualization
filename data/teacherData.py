595d02806b6f519bb32aaa66a60d339bc11a7e14
MuhammedMiiRanna <merali.med@gmail.com>
2022-12-18 04:10:01 +0100

    Update data files

--- data/teacherData.py
+++ data/teacherData.py
@@ -1,4 +1,5 @@
 import json
+# Teachers Data Extraction
 
 
 def session_info(day, session, section, module):
@@ -10,7 +11,28 @@ def session_info(day, session, section, module):
         'module': module
     }
 
-def 
+
+def add_session(is_class, grp=None):
+    if is_class:
+        # means cour
+        teachers[session['cours']['prof']] = teachers.get(
+            session['cours']['prof'], {})
+        teachers[session['cours']['prof']
+                 ][day] = teachers[session['cours']['prof']].get(day, {})
+        teachers[session['cours']['prof']][day][session_index] = teachers[session['cours']
+                                                                          ['prof']][day].get(session_index, {})
+        teachers[session['cours']['prof']][day][session_index] = session_info(
+            day, session['cours']['Time'], data_file_name, session['cours']['Subject'])
+        return
+    teachers[session['groups'][grp]['prof']] = teachers.get(
+        session['groups'][grp]['prof'], {})
+    teachers[session['groups'][grp]['prof']
+             ][day] = teachers[session['groups'][grp]['prof']].get(day, {})
+    teachers[session['groups'][grp]['prof']][day][session_index] = teachers[session['groups']
+                                                                            [grp]['prof']][day].get(session_index, {})
+    teachers[session['groups'][grp]['prof']][day][session_index] = session_info(
+        day, session['groups'][grp]['Time'], data_file_name + " " + grp, session['groups'][grp]['Subject'])
+
 
 global hours
 global teachers
@@ -35,30 +57,21 @@ teachers = {}
 for data_file_name in data_files:
     with open("data/"+data_file_name+".json", "r", encoding="UTF-8") as file:
         data = file.readlines()
-        data = json.loads("".join(data))['days'] # loads and get only "days" data
+        # loads and get only "days" data
+        data = json.loads("".join(data))['days']
 
     for day, sessions in data.items():
         for session_index, session in enumerate(sessions):
             if len(session['cours']):  # case if it's normal session
-                # teachers[session['cours']][day][hours[session['cours']["Time"]]] = session['cours']["Time"]
-                teachers[session['cours']['prof']] = teachers.get(session['cours']['prof'], {})
-                teachers[session['cours']['prof']][day] = teachers[session['cours']['prof']].get(day, {})
-                teachers[session['cours']['prof']][day][session_index] = teachers[session['cours']['prof']][day].get(session_index, {})
-                teachers[session['cours']['prof']][day][session_index] = session_info(day, session['cours']['Time'], data_file_name, session['cours']['Subject'], hours)
-
-                pass
-            elif len(session['groups']):
-                # # case if it's TD/TP session
+                add_session(True)
+            elif len(session['groups']):  # case if it's TD/TP session
                 for grp in session['groups']:
-                #     teachers.get(grp['prof'], {day: {}}).get(
-                #         grp['Time'], {hours[grp['Time']]: grp['Time']})
-
-                #     # hours[session_index] <==> grp['Time']
-                pass
-            else:  # case of empty session
+                    add_session(False, grp)
+            else:  # case of empty session(pass)
                 pass
 
     with open("data/teachersData.json", "w", encoding="UTF-8") as teachers_file:
         teachers_file.writelines(json.dumps(
             teachers, indent=4, ensure_ascii=False))
+
 print('>> Done!!')
